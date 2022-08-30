a756598194eea9e281f5d65930e0cceaf6147a32
Jonas Praast <Jonas Praast@DESKTOP-DPPLJFU>
2022-08-30 20:13:18 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Rückgaben_aus_unterschiedlichen_Klassen;
+
+namespace rauk {
+    class Program {
+        static void Main() {
+            Console.WriteLine("Hallo!\nWie heißen Sie?");
+            string name = Console.ReadLine().ToString();
+            Console.WriteLine("Guten Tag, " + Verarbeitung.Anrede(name) + name + "!\nWie Alt sind Sie?\n");
+            try {
+                int Alter = Convert.ToInt32(Console.ReadLine());
+                if(Verarbeitung.Alter(Alter) == true) {
+                    Console.WriteLine("Guten Tag, " + Verarbeitung.Anrede(name) + name + "! Sie dürfen dieses Programm benutzen!");
+                } else {
+                    Console.WriteLine(Verarbeitung.Anrede(name) + name + ", Sie sind nicht dazu berechtigt, dieses Programm zu nutzen!");
+                }
+                Console.Read();
+            } catch (Exception ex) {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Read();
+                Main();
+            }
+        }
+    }
+}
---
+++ Verarbeitung.cs
@@ -0,0 +1,22 @@
+﻿namespace Rückgaben_aus_unterschiedlichen_Klassen {
+    internal class Verarbeitung {
+        internal static bool Alter(int a) {
+            int TempAlter = a - 18;
+            if(TempAlter >= 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        internal static string Anrede(string a) {
+            if(a.Contains("Hansen") || a.Contains("Peters")) {
+                return "Frau ";
+            } else if (a.Contains("Meyer") || a.Contains("Andresen") || a.Contains("Scheel") || a.Contains("Müller")) {
+                return "Herr ";
+            } else {
+                return "Frau / Herr ";
+            }
+        }
+    }
+}
